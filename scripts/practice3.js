6b90333e0a87f3e45ca26aadc986927a3232f4a5
pogmyer <pogmyer@yahoo.com>
2023-06-04 20:20:51 -0500

    practice3

--- scripts/practice3.js
+++ scripts/practice3.js
@@ -1,6 +1,6 @@
-const insert = document.querySelector("#insert");
-insert.addEventListener("click", () => {
-  const subject = document.querySelector("#subject");
+const insert = document.querySelector("#insert");                 // from the Insert HTML button
+insert.addEventListener("click", () => {                          // on click of the Insert HTML button do ...
+  const subject = document.querySelector("#subject");             // make the "subject" the choice fron the list ??
   const positionSelect = document.querySelector("#position");
   subject.insertAdjacentHTML(
     positionSelect.value,
